4b5a54193e781ce1c0c30523a42db0c4b3a44bcd
Li Chaoyi <icylydia@gmail.com>
2018-03-16 16:18:51 +0900

    sketch

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by ebuser on 2018/03/16.
+//  Copyright © 2018 ebuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
