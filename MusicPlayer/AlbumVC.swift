f8ce4b6a150231996ae4e9b2b3f2c5bb8014f93e
李超逸 <icylydia@gmail.com>
2018-03-17 23:51:01 +0900

    song detail

--- MusicPlayer/AlbumVC.swift
+++ MusicPlayer/AlbumVC.swift
@@ -12,8 +12,18 @@ class AlbumVC: UITableViewController {
 
     var album: Album!
 
+    @IBOutlet weak var albumThumbImageView: UIImageView!
+    @IBOutlet weak var albumNameLabel: UILabel!
+    @IBOutlet weak var albumArtistLabel: UILabel!
+    @IBOutlet weak var albumPublishLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        albumNameLabel.text = album.name
+        albumArtistLabel.text = album.artist
+        albumPublishLabel.text = album.publish
+        albumThumbImageView.image = UIImage(named: album.cover)
     }
 
     // MARK: - Table view data source
@@ -26,6 +36,19 @@ class AlbumVC: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return album.songs.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "songCell", for: indexPath)
+        
+        if let cell = cell as? SongCell {
+            let song = album.songs[indexPath.row]
+            cell.serialLabel.text = "\(indexPath.row + 1)"
+            cell.nameLabel.text = song.name
+            cell.artistLabel.text = album.artist
+        }
+        
+        return cell
     }
 
 }
---
+++ MusicPlayer/SongCell.swift
@@ -0,0 +1,17 @@
+//
+//  SongCell.swift
+//  MusicPlayer
+//
+//  Created by 李超逸 on 2018/03/17.
+//  Copyright © 2018 ebuser. All rights reserved.
+//
+
+import UIKit
+
+class SongCell: UITableViewCell {
+
+    @IBOutlet weak var serialLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    
+}
