25b8a3c97defeef8e4c59aa89d3a89333b45cf3b
李超逸 <icylydia@gmail.com>
2018-03-24 22:46:08 +0900

    player core

--- MusicPlayer/PlaySongVC.swift
+++ MusicPlayer/PlaySongVC.swift
@@ -10,8 +10,46 @@ import UIKit
 
 class PlaySongVC: UIViewController {
 
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Player.main.prepareAndPlay()
+        Player.main.play()
+        Player.main.delegate = self
+        
+        durationLabel.text = Player.main.duration.minutFormat
     }
 
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        if Player.main.isPlaying {
+            Player.main.pause()
+            sender.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+        } else {
+            Player.main.play()
+            sender.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+        }
+    }
+}
+
+extension PlaySongVC: PlayerDelegate {
+    
+    func player(_ player: Player, onProgress progress: Float) {
+        progressView.progress = progress
+    }
+    
+    func player(_ player: Player, onTime currentTime: Int) {
+        currentTimeLabel.text = currentTime.minutFormat
+    }
+}
+
+extension Int {
+    var minutFormat: String {
+        let minute = self / 60
+        let second = self % 60
+        return String(format: "%d:%02d", minute, second)
+    }
 }
